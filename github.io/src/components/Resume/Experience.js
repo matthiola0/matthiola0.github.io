87cd2d47e2d4f0d20c717b4e3bd0e7d6238e0a4d
matthiola0 <abc35100762@gmail.com>
2026-04-28 09:53:59 +0800

    Sort Experience entries by date descending

--- github.io/src/components/Resume/Experience.js
+++ github.io/src/components/Resume/Experience.js
@@ -3,17 +3,25 @@ import PropTypes from 'prop-types';
 
 import Job from './Experience/Job';
 
-const Experience = ({ data }) => (
-  <div className="experience">
-    <div className="link-to" id="experience" />
-    <div className="title">
-      <h3>Experience</h3>
+const Experience = ({ data }) => {
+  const sorted = [...data].sort((a, b) => {
+    const aDate = a.endDate || a.startDate || '';
+    const bDate = b.endDate || b.startDate || '';
+    return bDate.localeCompare(aDate);
+  });
+
+  return (
+    <div className="experience">
+      <div className="link-to" id="experience" />
+      <div className="title">
+        <h3>Experience</h3>
+      </div>
+      {sorted.map((job) => (
+        <Job data={job} key={`${job.name}-${job.position}`} />
+      ))}
     </div>
-    {data.map((job) => (
-      <Job data={job} key={`${job.name}-${job.position}`} />
-    ))}
-  </div>
-);
+  );
+};
 
 Experience.propTypes = {
   data: PropTypes.arrayOf(
