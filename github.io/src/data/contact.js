081c35691c02038acd74472934f136b3e2e29c4a
matthiola0 <abc35100762@gmail.com>
2026-04-28 11:31:32 +0800

    Hide locked-out social icons (Facebook, Instagram, Twitter)

--- github.io/src/data/contact.js
+++ github.io/src/data/contact.js
@@ -1,9 +1,9 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
+// import { faFacebookF } from '@fortawesome/free-brands-svg-icons/faFacebookF';
+// import { faInstagram } from '@fortawesome/free-brands-svg-icons/faInstagram';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 // import { faAngellist } from '@fortawesome/free-brands-svg-icons/faAngellist';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+// import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
 // to add other icons.
@@ -14,26 +14,26 @@ const data = [
     label: 'Github',
     icon: faGithub,
   },
-  {
-    link: 'https://www.facebook.com/Pan.boy.10/',
-    label: 'Facebook',
-    icon: faFacebookF,
-  },
-  {
-    link: 'https://www.instagram.com/pan_boy1/',
-    label: 'Instagram',
-    icon: faInstagram,
-  },
+  // {
+  //   link: 'https://www.facebook.com/Pan.boy.10/',
+  //   label: 'Facebook',
+  //   icon: faFacebookF,
+  // },
+  // {
+  //   link: 'https://www.instagram.com/pan_boy1/',
+  //   label: 'Instagram',
+  //   icon: faInstagram,
+  // },
   {
     link: 'https://www.linkedin.com/in/po-yu-pan-0463a6350',
     label: 'LinkedIn',
     icon: faLinkedinIn,
   },
-  {
-    link: 'https://x.com/PanBoy1021',
-    label: 'Twitter',
-    icon: faTwitter,
-  },
+  // {
+  //   link: 'https://x.com/PanBoy1021',
+  //   label: 'Twitter',
+  //   icon: faTwitter,
+  // },
   {
     link: 'mailto:hi@matthiola.dev',
     label: 'Email',
